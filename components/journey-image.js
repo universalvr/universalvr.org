653f9f1aebb3deef09ba59864c41df2576196416
Chris Abrams <mail@chrisabrams.com>
2021-12-10 22:37:12 -0600

    add user journey images

---
+++ components/journey-image.js
@@ -0,0 +1,28 @@
+import breakpoints from './utils/breakpoints'
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledJourneyContainer = styled.div`
+  align-content: center;
+  align-items: center;
+  display: flex;
+  flex: 1 1 100%;
+  justify-content: center;
+`
+
+const StyledJourneyImg = styled.img`
+  display: flex;
+  max-width: 400px;
+
+  @media only screen and ${breakpoints.device.sm} {
+    max-width: 768px;
+  }
+`
+
+export default function JourneyImage({ alt, uri }) {
+  return (
+    <StyledJourneyContainer>
+      <StyledJourneyImg alt={alt} src={uri} />
+    </StyledJourneyContainer>
+  )
+}
--- pages/users/journeys/alex.js
+++ pages/users/journeys/alex.js
@@ -1,6 +1,7 @@
 import BackTo from '../../../components/back-to'
 import Container from '../../../components/container'
 import H1 from '../../../components/typography/h1'
+import JourneyImage from '../../../components/journey-image'
 import P from '../../../components/typography/p'
 import React from 'react'
 
@@ -8,6 +9,7 @@ export default function UserJourneysPage() {
   return (
     <Container>
       <H1>User Journey: Alex</H1>
+      <JourneyImage alt="Journey Image for Closed Captions" uri="/img/captions_border.png" />
       <P>Alex’s possible experiences with VR show how closed captions can enable a user to play a game. Without these captions, Alex can not successfully interact with the virtual environment. The fourth principle of Universal Design, perceptible information, ensures that all necessary information is provided to a user no matter what their abilities are. Information is necessary for any experience and should be provided in multiple means.</P>
       <BackTo href="/users/journeys" label="User Journeys" />
     </Container>
